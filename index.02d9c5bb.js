e7622a77335622e1068b43365ea7f6f681c58063
AnnaZ0203 <annazhoga0203@gmail.com>
2025-06-11 20:16:08 +0000

    Deploying to gh-pages from @ VladyslavYermakov/IceCream@6a0f2414b210179e55db7518a2d667e5acd0e3bd 🚀

---
+++ index.02d9c5bb.js
@@ -0,0 +1,2 @@
+(()=>{const o={openMobBtn:document.querySelector("[data-mob-open]"),closeMobBtn:document.querySelector("[data-mob-close]"),mob:document.querySelector("[data-mob]")};function e(){o.mob.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openMobBtn.addEventListener("click",e),o.closeMobBtn.addEventListener("click",e)})();
+//# sourceMappingURL=index.02d9c5bb.js.map
---
+++ index.1a7127b7.js
@@ -0,0 +1,2 @@
+!function(){var o={openMobBtn:document.querySelector("[data-mob-open]"),closeMobBtn:document.querySelector("[data-mob-close]"),mob:document.querySelector("[data-mob]")};function e(){o.mob.classList.toggle("is-hidden"),document.body.classList.toggle("no-scroll")}o.openMobBtn.addEventListener("click",e),o.closeMobBtn.addEventListener("click",e)}();
+//# sourceMappingURL=index.1a7127b7.js.map
